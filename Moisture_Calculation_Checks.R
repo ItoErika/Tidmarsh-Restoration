b6b79b289a37b9f9765bc727d2e5fcf45fd00b52
Erika Ito <etito@wisc.edu>
2017-10-20 14:30:04 -0400

    add autocorrelation plots

--- Moisture_Calculation_Checks.R
+++ Moisture_Calculation_Checks.R
@@ -1,84 +1,3 @@
-# Set the working directory
-setwd("C:/Users/erikai94/Documents/UMass/Tidmarsh/Calculation_Checks")
-
-######################################################### West (cm) ###################################################################
-# Open csv for West (cm)
-West_cm_Moisture<-read.csv("West_cm_Check.csv")
-
-# Fix colnames
-colnames(West_cm_Moisture)<-c("Meas_No", "Distance_(cm)", "X", "Permittivity_(mV)", "Permittivity_(V)", "Soil_Moisture_Mineral_(%)", "Soil_Moisture_Mineral_Calculated_(%)", "Latitude_(deg)", "Longitude_(deg)", "Misc1", "Misc2")
-
-# Make sure the mV to V conversion is correct:
-West_cm_Moisture[,"Permittivity_Check"]<-West_cm_Moisture[,"Permittivity_(mV)"]/1000
-
-# Add approximated values in between data from probe measurements:
-# Identify which rows have data
-Measured<-which(!(is.na((West_cm_Moisture[,"Permittivity_Check"]))))
-# Identify clusters of blanks
-# Take difference of each measured element and element ahead
-# Can identify the start of data gaps as places where the next measured point is more than one element away
-# LastMeasured represent the last element before a patch of missing data
-LastMeasured<-Measured[which(diff(Measured)>1)]
-# FirstMeasured represent the first element after patch of missing data
-# This is the element in Measured AFTER the elements where the next data point are more than one element away
-FirstMeasured<-Measured[which(diff(Measured)>1)+1]
-
-# Write a for loop that linearly interpolates values in these blank clusters 
-# Note that there are 36 clusters of blanks ( length(FirstMeasured) = length(LastMeasured) = 36)
-for (i in 1:36){
-    West_cm_Moisture[LastMeasured[i]:FirstMeasured[i],"Permittivity_Check"]<-seq(West_cm_Moisture[LastMeasured[i],"Permittivity_Check"], West_cm_Moisture[FirstMeasured[i],"Permittivity_Check"],length=length(LastMeasured[i]:FirstMeasured[i]))
-    }
-
-# Round the original permittivity column AND the checked permittivity column to 5 decimal places
-West_cm_Moisture[,"Permittivity_(V)"]<-round(West_cm_Moisture[,"Permittivity_(V)"], digits=5)
-West_cm_Moisture[,"Permittivity_Check"]<-round(West_cm_Moisture[,"Permittivity_Check"], digits=5)
-
-# Check to see if these columns are identical
-identical(West_cm_Moisture[,"Permittivity_(V)"], West_cm_Moisture[,"Permittivity_Check"])
-which(West_cm_Moisture[,"Permittivity_(V)"]-West_cm_Moisture[,"Permittivity_Check"]!=0)
-# NOTE:
-West_cm_Moisture[572:591,c("Permittivity_(V)", "Permittivity_Check")]
-
-######################################################### West (m) ####################################################################
-# Open csv for West (m) moisture data
-West_m_Moisture<-read.csv("West_m_Check.csv")
-# Fix colnames
-colnames(West_m_Moisture)<-c("Meas_No", "Distance_(cm)", "Permittivity_(mV)", "Permittivity_(V)", "Soil_Moisture_Mineral_(%)", 
-"Soil_Moisture_Mineral_Calculated_(%)", "Latitude_(deg)", "Longitude_(deg)", "Autocovariance_1m", "Autocovariance_2m", "Autocovariance_3m",
-"Autocovariance_4m", "Autocovariance_5m", "Autocovariance_6m", "Autocovariance_7m", "Autocovariance_8m", "Autocovariance_9m",  "h=1",
-"Autocorrelation_1","Autocorrelation_2","Autocorrelation_3","Autocorrelation_4","Autocorrelation_5","Autocorrelation_6",
-"Autocorrelation_7", "Autocorrelation_8", "Misc1", "Misc2")
-
- # Check V calculation:
-round(West_m_Moisture[,"Permittivity_(V)"], digits=4)-round(West_m_Moisture[,"Permittivity_(mV)"]/1000, digits=4)
-
-# Mineral calculation check:
-a0<-1.600
-a1<-8.400
-Mineral_Check<-sapply(West_m_Moisture[,"Permittivity_(V)"], function(x, y, z) ((1+6.175*x+6.303*x^2-73.578*x^3+183.44*x^4-184.78*x^5+68.017*x^6)-y)/z, a0, a1)
-# CHECK:
-round(Mineral_Check, digits=3)-West_m_Moisture[,"Soil_Moisture_Mineral_Calculated_(%)"]                                  
-                      
-# Write a function to get the autocovariance for lags 1-9
-autoCov<-function(lag, MoistureData) {
-    # Calculate average soil moisture
-    SM_mean<-mean(MoistureData)
-    # Record number of soil moisture measurements
-    n<-length(MoistureData)
-    # Make empty vector for autocovariance check
-    AutoCov<-vector(length=length(MoistureData)-1)                    
-    for (i in (lag+1):n){
-    AutoCov[i]<-(MoistureData[i]-SM_mean)*(MoistureData[i-lag]-SM_mean)
-    }
-    # Final step is to sum all of the columns and multipy them by 1/n
-    return(1/n*(sum(AutoCov)))
-    } 
-                                                             
-AutoCov<-sapply(1:9, function(x,y) autoCov(x,y), West_m_Moisture[,"Soil_Moisture_Mineral_Calculated_(%)"])                         
- 
-# Autocovariance function in R gives the same answers ast the function above:                
-acf(West_m_Moisture[,"Soil_Moisture_Mineral_Calculated_(%)"], lag.max = 9, type ="covariance", plot = FALSE)
-                
 #########################################################################################################################
                
 # Load Raw Data
@@ -182,8 +101,17 @@ MoistureData[6,"AutoCor_cm_lag5"]<-acv5/acv0
 MoistureData[7,"AutoCor_cm_lag6"]<-acv6/acv0                               
 MoistureData[8,"AutoCor_cm_lag7"]<-acv7/acv0                               
 MoistureData[9,"AutoCor_cm_lag8"]<-acv8/acv0                               
-MoistureData[10,"AutoCor_cm_lag9"]<-acv9/acv0                               
-
+MoistureData[10,"AutoCor_cm_lag9"]<-acv9/acv0 
+                
+# Create a vector of each autocorrelation coefficient for lags 0-9
+Autocorrelation<-c(acv0/acv0, acv1/acv0, acv2/acv0, acv3/acv0, acv4/acv0, acv5/acv0, acv6/acv0, acv7/acv0, acv8/acv0, acv9/acv0)                    
+# Create a vector of lags 0-9
+Lag<-0:9  
+# Calculate the 95% confidence bounds for the plot               
+conf<-1.96/sqrt(length(MoistureData[cm_Rows,"cm_Soil_Moisture_Calculated_(%)"]))
+# Create an autocorrelation plot 
+plot(Lag, AutoCorrelation, abline(h=conf, lty=3), ylim=c(0,1), main="West (cm) Autocorrelation Plot")                
+                
 # Get moisture data for every meter
 # Create a vector for every meter (every 100 cm)
 m_Distances<-seq(0, max(MoistureData[,"Distance_(cm)"]), 100) 
@@ -259,5 +187,99 @@ MoistureData[51,"AutoCor_m_lag5"]<-acv5/acv0
 MoistureData[61,"AutoCor_m_lag6"]<-acv6/acv0                               
 MoistureData[71,"AutoCor_m_lag7"]<-acv7/acv0                               
 MoistureData[81,"AutoCor_m_lag8"]<-acv8/acv0                               
-MoistureData[91,"AutoCor_m_lag9"]<-acv9/acv0                    
+MoistureData[91,"AutoCor_m_lag9"]<-acv9/acv0            
+
+# Create a vector of each autocorrelation coefficient for lags 0-9
+Autocorrelation<-c(acv0/acv0, acv1/acv0, acv2/acv0, acv3/acv0, acv4/acv0, acv5/acv0, acv6/acv0, acv7/acv0, acv8/acv0, acv9/acv0)                    
+# Create a vector of lags 0-9
+Lag<-0:9  
+# Calculate the 95% confidence bounds for the plot               
+conf<-1.96/sqrt(length(MoistureData[m_Rows,"m_Soil_Moisture_Calculated_(%)"]))
+# Create an autocorrelation plot 
+plot(Lag, AutoCorrelation, abline(h=conf, lty=3), ylim=c(0,1), main="West (m) Autocorrelation Plot")               
+######################################################### Old Script ###################################################################
+
+
+# Set the working directory
+setwd("C:/Users/erikai94/Documents/UMass/Tidmarsh/Calculation_Checks")
+
+######################################################### West (cm) ###################################################################
+# Open csv for West (cm)
+West_cm_Moisture<-read.csv("West_cm_Check.csv")
+
+# Fix colnames
+colnames(West_cm_Moisture)<-c("Meas_No", "Distance_(cm)", "X", "Permittivity_(mV)", "Permittivity_(V)", "Soil_Moisture_Mineral_(%)", "Soil_Moisture_Mineral_Calculated_(%)", "Latitude_(deg)", "Longitude_(deg)", "Misc1", "Misc2")
+
+# Make sure the mV to V conversion is correct:
+West_cm_Moisture[,"Permittivity_Check"]<-West_cm_Moisture[,"Permittivity_(mV)"]/1000
+
+# Add approximated values in between data from probe measurements:
+# Identify which rows have data
+Measured<-which(!(is.na((West_cm_Moisture[,"Permittivity_Check"]))))
+# Identify clusters of blanks
+# Take difference of each measured element and element ahead
+# Can identify the start of data gaps as places where the next measured point is more than one element away
+# LastMeasured represent the last element before a patch of missing data
+LastMeasured<-Measured[which(diff(Measured)>1)]
+# FirstMeasured represent the first element after patch of missing data
+# This is the element in Measured AFTER the elements where the next data point are more than one element away
+FirstMeasured<-Measured[which(diff(Measured)>1)+1]
+
+# Write a for loop that linearly interpolates values in these blank clusters 
+# Note that there are 36 clusters of blanks ( length(FirstMeasured) = length(LastMeasured) = 36)
+for (i in 1:36){
+    West_cm_Moisture[LastMeasured[i]:FirstMeasured[i],"Permittivity_Check"]<-seq(West_cm_Moisture[LastMeasured[i],"Permittivity_Check"], West_cm_Moisture[FirstMeasured[i],"Permittivity_Check"],length=length(LastMeasured[i]:FirstMeasured[i]))
+    }
+
+# Round the original permittivity column AND the checked permittivity column to 5 decimal places
+West_cm_Moisture[,"Permittivity_(V)"]<-round(West_cm_Moisture[,"Permittivity_(V)"], digits=5)
+West_cm_Moisture[,"Permittivity_Check"]<-round(West_cm_Moisture[,"Permittivity_Check"], digits=5)
+
+# Check to see if these columns are identical
+identical(West_cm_Moisture[,"Permittivity_(V)"], West_cm_Moisture[,"Permittivity_Check"])
+which(West_cm_Moisture[,"Permittivity_(V)"]-West_cm_Moisture[,"Permittivity_Check"]!=0)
+# NOTE:
+West_cm_Moisture[572:591,c("Permittivity_(V)", "Permittivity_Check")]
+
+######################################################### West (m) ####################################################################
+# Open csv for West (m) moisture data
+West_m_Moisture<-read.csv("West_m_Check.csv")
+# Fix colnames
+colnames(West_m_Moisture)<-c("Meas_No", "Distance_(cm)", "Permittivity_(mV)", "Permittivity_(V)", "Soil_Moisture_Mineral_(%)", 
+"Soil_Moisture_Mineral_Calculated_(%)", "Latitude_(deg)", "Longitude_(deg)", "Autocovariance_1m", "Autocovariance_2m", "Autocovariance_3m",
+"Autocovariance_4m", "Autocovariance_5m", "Autocovariance_6m", "Autocovariance_7m", "Autocovariance_8m", "Autocovariance_9m",  "h=1",
+"Autocorrelation_1","Autocorrelation_2","Autocorrelation_3","Autocorrelation_4","Autocorrelation_5","Autocorrelation_6",
+"Autocorrelation_7", "Autocorrelation_8", "Misc1", "Misc2")
+
+ # Check V calculation:
+round(West_m_Moisture[,"Permittivity_(V)"], digits=4)-round(West_m_Moisture[,"Permittivity_(mV)"]/1000, digits=4)
+
+# Mineral calculation check:
+a0<-1.600
+a1<-8.400
+Mineral_Check<-sapply(West_m_Moisture[,"Permittivity_(V)"], function(x, y, z) ((1+6.175*x+6.303*x^2-73.578*x^3+183.44*x^4-184.78*x^5+68.017*x^6)-y)/z, a0, a1)
+# CHECK:
+round(Mineral_Check, digits=3)-West_m_Moisture[,"Soil_Moisture_Mineral_Calculated_(%)"]                                  
+                      
+# Write a function to get the autocovariance for lags 1-9
+autoCov<-function(lag, MoistureData) {
+    # Calculate average soil moisture
+    SM_mean<-mean(MoistureData)
+    # Record number of soil moisture measurements
+    n<-length(MoistureData)
+    # Make empty vector for autocovariance check
+    AutoCov<-vector(length=length(MoistureData)-1)                    
+    for (i in (lag+1):n){
+    AutoCov[i]<-(MoistureData[i]-SM_mean)*(MoistureData[i-lag]-SM_mean)
+    }
+    # Final step is to sum all of the columns and multipy them by 1/n
+    return(1/n*(sum(AutoCov)))
+    } 
+                                                             
+AutoCov<-sapply(1:9, function(x,y) autoCov(x,y), West_m_Moisture[,"Soil_Moisture_Mineral_Calculated_(%)"])                         
+ 
+# Autocovariance function in R gives the same answers ast the function above:                
+acf(West_m_Moisture[,"Soil_Moisture_Mineral_Calculated_(%)"], lag.max = 9, type ="covariance", plot = FALSE)
+                
+        
                 
