3ad613710a958615f618c2c514b2d6c1cad19096
Erika Ito <etito@wisc.edu>
2018-05-09 03:21:03 -0400

    Update DTS.m

--- DTS.m
+++ DTS.m
@@ -1,4 +1,182 @@
-% Some General Commands for plotting DTS Data
+%%%%%%%%%%%%%%%%%%%%%%% FIELD TRIP DATA %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%% Field 10 cm %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+load('TW_10cm.mat')
+
+% Remove mirrored part of plot
+% Remove negative distance values in DTS box in barn
+posdist=distance(distance>0);
+% Find the middle point in the positive distance data
+middist=length(posdist)/2;
+% Subset distance data to only include half of mirrored data
+% NOTE: remember to add the length of negative data to find the proper midpoint
+plotdist=distance(1:(length(distance(distance<=0))+middist));
+% Subset the temperature data matrix to only include the selected distances
+plottemp=tempC(1:length(plotdist),:);
+
+% Shift datenum by 4 hours to convert from GMT?
+datetimeshifted=[];
+for i=1:numel(datetime)
+datetimeshifted=[datetimeshifted;addtodate(datetime(i),-4,'hour')];
+end
+
+figure
+imagesc(plotdist, datetimeshifted(315:456), rot90(plottemp(:,315:456)))
+colorbar
+datetick('y', 2, 'keeplimits')
+caxis([0 15])
+title('Tidmarsh West Soil 10 cm (3/1/18-3/6/18)')
+ylabel('Date')
+xlabel('Distance (m)')
+set(get(colorbar,'label'),'string','Temperature (deg C)');
+set(gcf,'PaperOrientation','landscape');
+set(gcf, 'Units', 'inches', 'PaperPosition', [0,0,11.5,7.5]);
+print(gcf, '-dpdf', 'TW_DTS_Field_10cm_half.pdf');
+
+%%%%%%%%%%%%%%%%%%%%%%% Field 20 cm %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+load('TW_20cm.mat')
+% Remove mirrored part of plot
+% Remove negative distance values in DTS box in barn
+posdist=distance(distance>0)
+% Find the middle point in the positive distance data
+middist=length(posdist)/2
+% Subset distance data to only include half of mirrored data
+% NOTE: remember to add the length of negative data to find the proper midpoint
+plotdist=distance(1:(length(distance(distance<=0))+middist))
+% Subset the temperature data matrix to only include the selected distances
+plottemp=tempC(1:length(plotdist),:)
+
+% Shift datenum by 4 hours to convert from GMT?
+datetimeshifted=[];
+for i=1:numel(datetime)
+datetimeshifted=[datetimeshifted;addtodate(datetime(i),-4,'hour')];
+end
+
+figure
+imagesc(plotdist, datetimeshifted(315:456), rot90(plottemp(:,315:456)))
+colorbar
+datetick('y', 2, 'keeplimits')
+caxis([0 15])
+title('Tidmarsh West Soil 20 cm (3/1/18-3/6/18)')
+ylabel('Date')
+xlabel('Distance (m)')
+set(get(colorbar,'label'),'string','Temperature (deg C)');
+set(gcf,'PaperOrientation','landscape');
+set(gcf, 'Units', 'inches', 'PaperPosition', [0,0,11.5,7.5]);
+print(gcf, '-dpdf', 'TW_DTS_Field_20cm_half.pdf');
+
+%%%%%%%%%%%%%%%%%%%%%%% Field 30 cm %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+load('TW_30cm.mat')
+
+% Remove mirrored part of plot
+% Remove negative distance values in DTS box in barn
+posdist=distance(distance>0)
+% Find the middle point in the positive distance data
+middist=length(posdist)/2
+% Subset distance data to only include half of mirrored data
+% NOTE: remember to add the length of negative data to find the proper midpoint
+plotdist=distance(1:(length(distance(distance<=0))+middist))
+% Subset the temperature data matrix to only include the selected distances
+plottemp=tempC(1:length(plotdist),:)
+
+% Shift datenum by 4 hours to convert from GMT?
+datetimeshifted=[];
+for i=1:numel(datetime)
+datetimeshifted=[datetimeshifted;addtodate(datetime(i),-4,'hour')];
+end
+
+figure
+imagesc(plotdist, datetimeshifted(315:456), rot90(plottemp(:,315:456)))
+colorbar
+datetick('y', 2, 'keeplimits')
+caxis([0 15])
+title('Tidmarsh West Soil 30 cm (3/1/18-3/6/18)')
+ylabel('Date')
+xlabel('Distance (m)')
+set(get(colorbar,'label'),'string','Temperature (deg C)');
+set(gcf,'PaperOrientation','landscape');
+set(gcf, 'Units', 'inches', 'PaperPosition', [0,0,11.5,7.5]);
+print(gcf, '-dpdf', 'TW_DTS_Field_30cm_half.pdf');
+
+%%%%%%%%%%%%%%%%%%%%%%% Air Temp %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+load('TW_10cm.mat')
+
+% Remove mirrored part of plot
+% Remove negative distance values in DTS box in barn
+posdist=distance(distance>0);
+% Find the middle point in the positive distance data
+middist=length(posdist)/2;
+% Subset distance data to only include half of mirrored data
+% NOTE: remember to add the length of negative data to find the proper midpoint
+plotdist=distance(1:(length(distance(distance<=0))+middist));
+% Subset the temperature data matrix to only include the selected distances
+plottemp=tempC(1:length(plotdist),:);
+
+% Shift datenum by 4 hours to convert from GMT?
+datetimeshifted=[];
+for i=1:numel(datetime)
+datetimeshifted=[datetimeshifted;addtodate(datetime(i),-4,'hour')];
+end
+
+%196:221
+% Extract air temperature from the field trip dates
+airtemp=plottemp(length(plottemp),:)
+plot(datetimeshifted(363:408), airtemp(363:408), 'b-','linewidth',2)
+datetick('x', 13, 'keeplimits')
+% plot average air temperature
+meanair=mean(airtemp(363:408))*(ones(size(363:408)))
+plot(datetimeshifted(363:408), meanair, 'k--', 'linewidth', 2)
+ylim([4, 6.5])
+ylabel('Temperature (deg C)')
+xlabel('Time')
+title('Field Trip Air Temperature (3/3/18-3/4/18)')
+set(gcf,'PaperOrientation','landscape');
+set(gcf, 'Units', 'inches', 'PaperPosition', [0,0,11.5,7.5]);
+print(gcf, '-dpdf', 'TW_DTS_airtemp.pdf');
+
+%%%%%%%%%%%%%%%%%%%%%%% GW Temp %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%196:221
+% Extract groundwater temperature from the field trip date
+load('TW_30cm.mat')
+% Remove mirrored part of plot
+% Remove negative distance values in DTS box in barn
+posdist=distance(distance>0)
+% Find the middle point in the positive distance data
+middist=length(posdist)/2
+% Subset distance data to only include half of mirrored data
+% NOTE: remember to add the length of negative data to find the proper midpoint
+plotdist=distance(1:(length(distance(distance<=0))+middist))
+% Subset the temperature data matrix to only include the selected distances
+plottemp=tempC(1:length(plotdist),:)
+
+% Shift datenum by 4 hours to convert from GMT?
+datetimeshifted=[];
+for i=1:numel(datetime)
+datetimeshifted=[datetimeshifted;addtodate(datetime(i),-4,'hour')];
+end
+
+gwtemp=plottemp(3200,:)
+plot(datetimeshifted(363:408), gwtemp(363:408), 'b-','linewidth',2)
+hold
+datetick('x', 13, 'keeplimits')
+% plot average gw temperature
+meangw=mean(gwtemp(363:408))*(ones(size(363:408)));
+plot(datetimeshifted(363:408), meangw, 'k--', 'linewidth', 2)
+ylabel('Temperature (deg C)')
+xlabel('Time')
+ylim([6, 8.5])
+title('Field Trip Ground Water Temperature (3/3/18-3/4/18)')
+set(gcf,'PaperOrientation','landscape');
+set(gcf, 'Units', 'inches', 'PaperPosition', [0,0,11.5,7.5]);
+print(gcf, '-dpdf', 'TW_DTS_gwtemp_D.pdf');
+
+
+
+ Some General Commands for plotting DTS Data
 
 % This command will load the temperature file parsed from the individual data files
 % It contains reference temperature loggers (two external and one internal)
@@ -137,154 +315,10 @@ set(gcf,'PaperOrientation','landscape');
 set(gcf, 'Units', 'inches', 'PaperPosition', [0,0,11.5,7.5]);
 print(gcf, '-dpdf', 'TW_DTS_All_30cm_half.pdf');
 
-load('TW_10cm.mat')
-
-% Remove mirrored part of plot
-% Remove negative distance values in DTS box in barn
-posdist=distance(distance>0)
-% Find the middle point in the positive distance data
-middist=length(posdist)/2
-% Subset distance data to only include half of mirrored data
-% NOTE: remember to add the length of negative data to find the proper midpoint
-plotdist=distance(1:(length(distance(distance<=0))+middist))
-% Subset the temperature data matrix to only include the selected distances
-plottemp=tempC(1:length(plotdist),:)
-
-% Shift datenum by 4 hours to convert from GMT?
-datetimeshifted=[];
-for i=1:numel(datetime)
-datetimeshifted=[datetimeshifted;addtodate(datetime(i),-4,'hour')];
-end
-
-figure
-imagesc(plotdist, datetimeshifted(315:456), rot90(plottemp(:,315:456)))
-colorbar
-datetick('y', 2, 'keeplimits')
-caxis([0 15])
-title('Tidmarsh West Soil 10 cm (3/1/18-3/6/18)')
-ylabel('Date')
-xlabel('Distance (m)')
-set(get(colorbar,'label'),'string','Temperature (deg C)');
-set(gcf,'PaperOrientation','landscape');
-set(gcf, 'Units', 'inches', 'PaperPosition', [0,0,11.5,7.5]);
-print(gcf, '-dpdf', 'TW_DTS_Field_10cm_half.pdf');
-
-%196:221
-% Extract air temperature from the field trip dates
-airtemp=plottemp(length(plottemp),:)
-plot(datetimeshifted(363:408), airtemp(363:408), 'b-','linewidth',2)
-hold
-datetick('x', 13, 'keeplimits')
-% plot average air temperature
-meanair=mean(airtemp(363:408))*(ones(size(363:408)))
-plot(datetimeshifted(363:408), meanair, 'k--', 'linewidth', 2)
-ylim([4, 6.5])
-ylabel('Temperature (deg C)')
-xlabel('Time')
-title('Field Trip Air Temperature (3/3/18-3/4/18)')
-set(gcf,'PaperOrientation','landscape');
-set(gcf, 'Units', 'inches', 'PaperPosition', [0,0,11.5,7.5]);
-print(gcf, '-dpdf', 'TW_DTS_airtemp.pdf');
-
-%196:221
-% Extract groundwater temperature from the field trip date
-load('TW_30cm.mat')
-% Remove mirrored part of plot
-% Remove negative distance values in DTS box in barn
-posdist=distance(distance>0)
-% Find the middle point in the positive distance data
-middist=length(posdist)/2
-% Subset distance data to only include half of mirrored data
-% NOTE: remember to add the length of negative data to find the proper midpoint
-plotdist=distance(1:(length(distance(distance<=0))+middist))
-% Subset the temperature data matrix to only include the selected distances
-plottemp=tempC(1:length(plotdist),:)
-
-% Shift datenum by 4 hours to convert from GMT?
-datetimeshifted=[];
-for i=1:numel(datetime)
-datetimeshifted=[datetimeshifted;addtodate(datetime(i),-4,'hour')];
-end
-
-gwtemp=plottemp(3200,:)
-plot(datetimeshifted(315:456), gwtemp(315:456), 'b-','linewidth',2)
-hold
-datetick('x', 13, 'keeplimits')
-% plot average gw temperature
-meangw=mean(gwtemp(315:456))*(ones(size(315:456)));
-plot(datetimeshifted(315:456), meangw, 'k--', 'linewidth', 2)
-ylabel('Temperature (deg C)')
-xlabel('Time')
-ylim([5, 9.5])
-title('Field Trip Ground Water Temperature (3//18)')
-set(gcf,'PaperOrientation','landscape');
-set(gcf, 'Units', 'inches', 'PaperPosition', [0,0,11.5,7.5]);
-print(gcf, '-dpdf', 'TW_DTS_gwtemp.pdf');
-
-load('TW_20cm.mat')
-
-% Remove mirrored part of plot
-% Remove negative distance values in DTS box in barn
-posdist=distance(distance>0)
-% Find the middle point in the positive distance data
-middist=length(posdist)/2
-% Subset distance data to only include half of mirrored data
-% NOTE: remember to add the length of negative data to find the proper midpoint
-plotdist=distance(1:(length(distance(distance<=0))+middist))
-% Subset the temperature data matrix to only include the selected distances
-plottemp=tempC(1:length(plotdist),:)
-
-% Shift datenum by 4 hours to convert from GMT?
-datetimeshifted=[];
-for i=1:numel(datetime)
-datetimeshifted=[datetimeshifted;addtodate(datetime(i),-4,'hour')];
-end
-
-figure
-imagesc(plotdist, datetimeshifted(147:268), rot90(plottemp(:,147:268)))
-colorbar
-datetick('y', 2, 'keeplimits')
-caxis([-5 15])
-title('Tidmarsh West Soil 20 cm (2/22/18-2/26/18)')
-ylabel('Time')
-xlabel('Distance (m)')
-set(get(colorbar,'label'),'string','Temperature (deg C)');
-set(gcf,'PaperOrientation','landscape');
-set(gcf, 'Units', 'inches', 'PaperPosition', [0,0,11.5,7.5]);
-print(gcf, '-dpdf', 'TW_DTS_Field_20cm_half.pdf');
 
 
-load('TW_30cm.mat')
 
-% Remove mirrored part of plot
-% Remove negative distance values in DTS box in barn
-posdist=distance(distance>0)
-% Find the middle point in the positive distance data
-middist=length(posdist)/2
-% Subset distance data to only include half of mirrored data
-% NOTE: remember to add the length of negative data to find the proper midpoint
-plotdist=distance(1:(length(distance(distance<=0))+middist))
-% Subset the temperature data matrix to only include the selected distances
-plottemp=tempC(1:length(plotdist),:)
 
-% Shift datenum by 4 hours to convert from GMT?
-datetimeshifted=[];
-for i=1:numel(datetime)
-datetimeshifted=[datetimeshifted;addtodate(datetime(i),-4,'hour')];
-end
-
-figure
-imagesc(plotdist, datetimeshifted(147:268), rot90(plottemp(:,147:268)))
-colorbar
-datetick('y', 13, 'keeplimits')
-caxis([5 9.5])
-title('Tidmarsh West Soil 30 cm (2/22/18-2/26/18)')
-ylabel('Time')
-xlabel('Distance (m)')
-set(get(colorbar,'label'),'string','Temperature (deg C)');
-set(gcf,'PaperOrientation','landscape');
-set(gcf, 'Units', 'inches', 'PaperPosition', [0,0,11.5,7.5]);
-print(gcf, '-dpdf', 'TW_DTS_Field_30cm_half_test.pdf');
 
 
 
