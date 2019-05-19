e6766f3997fea5205a3faa9f638c16439183088c
Erika Ito <etito@wisc.edu>
2019-05-19 13:54:09 -0400

    Update model.py

--- model.py
+++ model.py
@@ -6,33 +6,26 @@ import numpy as np
 modelname='TW2D1'
 mf = flopy.modflow.Modflow(modelname,exe_name='mf2005')
 
-# Model domain and grid definition
-Lx = 1000.
-Ly = 1000.
-ztop = 0.
-zbot = -50.
-nlay = 1
-nrow = 10
-ncol = 10
-delr = Lx/ncol
-delc = Ly/nrow
-delv = (ztop - zbot) / nlay
-botm = np.linspace(ztop, zbot, nlay + 1)
-
 # CREATE MODEL GRID
 # Specify 2D model dimensions
-Lx = 20. # Aquifer length in x
-Ly = 10. # Aquifer length in y
-Lz = 1. # Aquifer length in z
+Lx = 20 # Aquifer length in x (m)
+Ly = 1 # Aquifer length in y
+Lz = 10 # Aquifer length in z / aquifer thickness (m)
 # Spatial discretization
 nlay = 50 # Number of grid layers
 nrow = 1 # Number of grid rows
 ncol = 100 # Number of grid columns
 delr = Lx / ncol # Spacing between columns
-delc = Ly 
+delc = Ly / nrow
 delv = Lz / nlay # Spacing between layers
 # Elevation for model surface and layer bottom elevation
 model_top = 10. # Elevation of model top 
 model_botm = np.linspace(model_top - delv, 0., nlay) # Elevation of model bottom
 
+# Define the DIS package
 dis = flopy.modflow.ModflowDis(mf, nlay, nrow, ncol, delr=delr, delc=delc, top=model_top, botm=model_botm)
+
+# Define variables for the BAS package
+ibound = np.ones((nlay, nrow, ncol), dtype=np.int32)
+ibound[:, :, -1] = -1
+bas = flopy.modflow.ModflowBas(swt, ibound, 0)
