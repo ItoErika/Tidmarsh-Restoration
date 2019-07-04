c6cb483158c2d79137f32641d375df03af5928ba
Erika Ito <etito@wisc.edu>
2019-07-03 20:54:31 -0400

    add pz 1 sand from 6/3/19

--- WaterLevels.R
+++ WaterLevels.R
@@ -333,8 +333,18 @@ TW_PZ_01_Jun319[which(TW_PZ_01_Jun319[,"Date_Time"]=="03/30/19 12:30:00 PM"),"m_
 TW_PZ_01_Jun319[which(TW_PZ_01_Jun319[,"Date_Time"]=="06/03/19 12:45:00 PM"),"m_manual"]<-0.106 
 # Save as CSV  
 write.csv(TW_PZ_01_Jun319, file="TWPZ01_11-20-18_to_6-3-19.csv", row.names=FALSE)                         
-              
+  
 
+############## TW_PZ_01_SAND ############## 
+            
+# Create a column for the depth to water below ground surface
+# The top of piezometer casing to ground surface = 130 cm
+TW_PZ_01_SAND_Jun319[,"m_below_GS"]<-152.9/100-(130/100+TW_PZ_01_SAND_Jun319[,"m_water"]) 
+# Add manual data
+TW_PZ_01_SAND_Jun319[which(TW_PZ_01_SAND_Jun319[,"Date_Time"]=="03/30/19 12:15:00 PM"),"m_manual"]<-0.07
+TW_PZ_01_SAND_Jun319[which(TW_PZ_01_SAND_Jun319[,"Date_Time"]=="06/03/19 01:00:00 PM"),"m_manual"]<-0.102
+write.csv(TW_PZ_01_SAND_Jun319, file="TWPZ01_SAND_11-20-18_to_6-3-19.csv", row.names=FALSE)                         
+                       
 ############## TW_PZ_02 ##############       
 # Create a column for the depth to water below ground surface
 TW_PZ_02_Nov[,"m_above_GS"]<-(TW_PZ_02_Nov[,"m_water"])-.6
@@ -366,7 +376,11 @@ TW_PZ_02_5_6[Start_Spike:Stop_Spike,"m_above_GS"]<-Smoothed_Spike
 TW_PZ_02_5_6[which(TW_PZ_02_5_6[,"Date_Time"]=="10/23/17 03:15:00 PM"),"m_manual"]<-0.384
 TW_PZ_02_5_6[which(TW_PZ_02_5_6[,"Date_Time"]=="03/03/18 01:00:00 PM"),"m_manual"]<-0.45  
 TW_PZ_02_5_6[which(TW_PZ_02_5_6[,"Date_Time"]=="03/24/18 06:15:00 AM"),"m_manual"]<-0.372 
-write.csv(TW_PZ_02_5_6, file="TWPZ02_8-10-17_to_3-24-18.csv", row.names=FALSE)   
+write.csv(TW_PZ_02_5_6, file="TWPZ02_8-10-17_to_3-24-18.csv", row.names=FALSE)  
+
+# Create a column for the depth to water below ground surface
+# The top of piezometer casing to ground surface = 19 cm
+TW_PZ_02_Jun319[,"m_above_GS"]<-(TW_PZ_02_Jun319[,"m_water"])-.64 
     
 ############## TW_PZ_05 ##############       
 # Create a column for the depth to water below ground surface
@@ -707,6 +721,15 @@ ggsave("TW_PZ_01_11-20-18_to_6-3-19.pdf", width = 12, height = 6)
 Plot_Times<-as.POSIXct(TW_PZ_01_Jun319[,"Date_Time"], "%m/%d/%y %I:%M:%S %p", tz="America/New_York")
 ggplot(TW_PZ_01_Jun319, aes(Plot_Times, TW_PZ_01_Jun319[,"m_below_GS"]))+geom_line(color='royalblue3', size=.6) + xlab("Date") + ylab("Depth to Water Below Ground Surface (m)")+ggtitle("TW_PZ_01")+  scale_x_datetime(breaks = seq(Plot_Times[1], Plot_Times[length(Plot_Times)], "7 days"),date_labels="%b %d")+ scale_y_reverse(limits =c(.3,-.1)) +theme(axis.text.x = element_text(angle=45, vjust = 0.5))+ geom_point(aes(x=Plot_Times, y=TW_PZ_01_Jun319[,"m_manual"]), color="orange3", size=3)                            
 ggsave("TW_PZ_01_11-20-18_to_6-3-19_manual.pdf", width = 12, height = 6)
+
+#### TW_PZ_01_SAND ####
+Plot_Times<-as.POSIXct(TW_PZ_01_SAND_Jun319[,"Date_Time"], "%m/%d/%y %I:%M:%S %p", tz="America/New_York")
+ggplot(TW_PZ_01_SAND_Jun319, aes(Plot_Times, TW_PZ_01_SAND_Jun319[,"m_below_GS"]))+geom_line(color='royalblue3', size=.6) + xlab("Date") + ylab("Depth to Water Below Ground Surface (m)")+ggtitle("TW_PZ_01_SAND")+  scale_x_datetime(breaks = seq(Plot_Times[1], Plot_Times[length(Plot_Times)], "7 days"),date_labels="%b %d")+ scale_y_reverse(limits =c(.3,-.3)) +theme(axis.text.x = element_text(angle=45, vjust = 0.5))
+ggsave("TW_PZ_01_SAND_11-20-18_to_6-3-19.pdf", width = 12, height = 6)  
+# With manual
+Plot_Times<-as.POSIXct(TW_PZ_01_SAND_Jun319[,"Date_Time"], "%m/%d/%y %I:%M:%S %p", tz="America/New_York")
+ggplot(TW_PZ_01_SAND_Jun319, aes(Plot_Times, TW_PZ_01_SAND_Jun319[,"m_below_GS"]))+geom_line(color='royalblue3', size=.6) + xlab("Date") + ylab("Depth to Water Below Ground Surface (m)")+ggtitle("TW_PZ_01_SAND")+  scale_x_datetime(breaks = seq(Plot_Times[1], Plot_Times[length(Plot_Times)], "7 days"),date_labels="%b %d")+ scale_y_reverse(limits =c(.3,-.3)) +theme(axis.text.x = element_text(angle=45, vjust = 0.5))+ geom_point(aes(x=Plot_Times, y=TW_PZ_01_SAND_Jun319[,"m_manual"]), color="orange3", size=3)                            
+ggsave("TW_PZ_01_SAND_11-20-18_to_6-3-19_manual.pdf", width = 12, height = 6)  
 
 # TW_PZ_02
 Plot_Times<-as.POSIXct(TW_PZ_02_5_6[,"Date_Time"], "%m/%d/%y %I:%M:%S %p", tz="America/New_York")
