1e1956fc4044a2f87f3901c4e163864ba18fd172
Erika Ito <etito@wisc.edu>
2019-08-02 16:33:06 -0400

    start with example sisim

--- geostatsSISIM.py
+++ geostatsSISIM.py
@@ -1,1 +1,59 @@
+# Example taken from: https://github.com/GeostatsGuy/PythonNumericalDemos/blob/master/GeostatsPy_sisim.ipynb
 
+import os                                                 
+import numpy as np                                      
+import pandas as pd                                       
+import matplotlib.pyplot as plt                           
+import geostatspy.GSLIB as GSLIB
+
+import geostatspy.geostats as geostats
+
+
+df = pd.read_csv("sample_data_MV_biased.csv")             # read a .csv file in as a DataFrame
+df.describe()                                             # summary statistics 
+df = df.sample(50)                                        # extract 50 samples
+df = df.reset_index()                                     # reset the record index 
+df.head(n=20)
+
+
+# Sequential Indicator Simulation with Simple Kriging Multiple Realizations 
+nx = 100; ny = 100; xsiz = 10.0; ysiz = 10.0; xmn = 5.0; ymn = 5.0; nxdis = 1; nydis = 1
+ndmin = 0; ndmax = 10; nodmax = 10; radius = 400; skmean = 0
+tmin = -999; tmax = 999
+dummy_trend = np.zeros((10,10))            # the current version requires trend input - if wrong size it is ignored 
+
+ncut = 2                                   # number of facies
+thresh = [0,1]                             # the facies categories (use consisten order)
+gcdf = [0.4,0.6]                           # the global proportions of the categories
+varios = []                                # the variogram list
+varios.append(GSLIB.make_variogram(nug=0.0,nst=1,it1=1,cc1=1.0,azi1=0,hmaj1=100,hmin1=100)) # shale indicator variogram
+varios.append(GSLIB.make_variogram(nug=0.0,nst=1,it1=1,cc1=1.0,azi1=0,hmaj1=100,hmin1=100)) # sand indicator variogram
+
+sim_ik1 = geostats.sisim(df,'X','Y','Facies',ivtype=0,koption=0,ncut=2,thresh=thresh,gcdf=gcdf,trend=dummy_trend,
+               tmin=tmin,tmax=tmax,zmin=0.0,zmax=1.0,ltail=1,ltpar=1,middle=1,mpar=0,utail=1,utpar=2,
+               nx=nx,xmn=xmn,xsiz=xsiz,ny=ny,ymn=ymn,ysiz=ysiz,seed = 73073,
+               ndmin=ndmin,ndmax=ndmax,nodmax=nodmax,mults=1,nmult=3,noct=-1,radius=radius,ktype=0,vario=varios)
+
+sim_ik2 = geostats.sisim(df,'X','Y','Facies',ivtype=0,koption=0,ncut=2,thresh=thresh,gcdf=gcdf,trend=dummy_trend,
+               tmin=tmin,tmax=tmax,zmin=0.0,zmax=1.0,ltail=1,ltpar=1,middle=1,mpar=0,utail=1,utpar=2,
+               nx=nx,xmn=xmn,xsiz=xsiz,ny=ny,ymn=ymn,ysiz=ysiz,seed = 73074,
+               ndmin=ndmin,ndmax=ndmax,nodmax=nodmax,mults=1,nmult=3,noct=-1,radius=radius,ktype=0,vario=varios)
+
+sim_ik3 = geostats.sisim(df,'X','Y','Facies',ivtype=0,koption=0,ncut=2,thresh=thresh,gcdf=gcdf,trend=dummy_trend,
+               tmin=tmin,tmax=tmax,zmin=0.0,zmax=1.0,ltail=1,ltpar=1,middle=1,mpar=0,utail=1,utpar=2,
+               nx=nx,xmn=xmn,xsiz=xsiz,ny=ny,ymn=ymn,ysiz=ysiz,seed = 73075,
+               ndmin=ndmin,ndmax=ndmax,nodmax=nodmax,mults=1,nmult=3,noct=-1,radius=radius,ktype=0,vario=varios)
+
+xmin = 0.0; xmax = 1000.0; ymin = 0.0; ymax = 1000.0; cmap = plt.cm.inferno # plotting parameters
+
+plt.subplot(131)                                          # plot the results
+GSLIB.locpix_st(sim_ik1,xmin,xmax,ymin,ymax,xsiz,-.4,1.0,df,'X','Y','Facies','Sequential Indicator Simulation - Realization 1','X(m)','Y(m)','Facies',cmap)
+
+plt.subplot(132)                                          # plot the results
+GSLIB.locpix_st(sim_ik2,xmin,xmax,ymin,ymax,xsiz,-.4,1.0,df,'X','Y','Facies','Sequential Indicator Simulation - Realization 2','X(m)','Y(m)','Facies',cmap)
+
+plt.subplot(133)                                          # plot the results
+GSLIB.locpix_st(sim_ik3,xmin,xmax,ymin,ymax,xsiz,-.4,1.0,df,'X','Y','Facies','Sequential Indicator Simulation - Realization 3','X(m)','Y(m)','Facies',cmap)
+
+plt.subplots_adjust(left=0.0, bottom=0.0, right=4.0, top=1.5, wspace=0.2, hspace=0.2)
+plt.show()
