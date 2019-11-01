7033bbf3feb177e3cd2c4ec5bb708a519d42fa53
Erika Ito <etito@wisc.edu>
2019-11-01 13:25:23 -0400

    Update SISIM.py

--- SISIM.py
+++ SISIM.py
@@ -46,10 +46,10 @@ def expand(x, y):
 ####################### H / 25 peat / 75 sand / No hummocks  #######################
 ####################################################################################
 # Sequential Indicator Simulation with Simple Kriging Multiple Realizations 
-# A: 0.7512 / 0.2488 = SAND / PEAT
-# B: 0.7464 / 0.25360000000000005
-# C: 0.7524 / 0.2477
-# D: 0.7422 / 0.2578    
+# A: 0.2488 / 0.7512  = PEAT / SAND
+# B: 0.2536 / 0.7464
+# C: 0.2477 / 0.7524
+# D: 0.2578 / 0.7422    
 # Original A:  0.7484 / 0.25160000000000005
 nx = 500; ny = 10; xsiz =1; ysiz = 1; xmn = 0.5; ymn = 0.5; nxdis = 1; nydis = 1
 ndmin = 0; ndmax = 10; nodmax = 10; radius =25; skmean = 0
@@ -150,6 +150,10 @@ plt.savefig('sim25flat_D.png', bbox_inches='tight', dpi=300, frameon='false', pa
 ####################### V / 25 peat / 75 sand / No hummocks  #######################
 ####################################################################################
 # Sequential Indicator Simulation with Simple Kriging Multiple Realizations 
+# A: 0.2452 / 0.7548 = PEAT / SAND
+# B: 0.2434 / 0.7566
+# C: 0.2540 / 0.7460
+
 nx = 500; ny = 10; xsiz =1; ysiz = 1; xmn = 0.5; ymn = 0.5; nxdis = 1; nydis = 1
 ndmin = 0; ndmax = 10; nodmax = 10; radius =25; skmean = 0
 tmin = -999; tmax = 999
@@ -228,6 +232,9 @@ plt.savefig('sim25flat_Cv.png', bbox_inches='tight', dpi=300, frameon='false', p
 ####################### H / 50 peat / 50 sand / No hummocks  #######################
 ####################################################################################
 # Sequential Indicator Simulation with Simple Kriging Multiple Realizations 
+# A: 0.5020 / 0.4980 = PEAT/SAND
+# B: 0.4933 / 0.5067
+# C: 0.5015 / 0.4985
 
 nx = 500; ny = 15; xsiz =1; ysiz = 1; xmn = 0.5; ymn = 0.5; nxdis = 1; nydis = 1
 ndmin = 0; ndmax = 10; nodmax = 10; radius =25; skmean = 0
@@ -306,6 +313,11 @@ plt.savefig('sim50flat_C.png', bbox_inches='tight', dpi=300, frameon='false', pa
 ####################################################################################
 ####################### V / 50 peat / 50 sand / No hummocks  #######################
 ####################################################################################
+# Sequential Indicator Simulation with Simple Kriging Multiple Realizations 
+# A: 0.4941 / 0.5059 = PEAT/SAND
+# B: 0.5083 / 0.4917
+# C: 0.4909 / 0.5091
+
 nx = 500; ny = 15; xsiz =1; ysiz = 1; xmn = 0.5; ymn = 0.5; nxdis = 1; nydis = 1
 ndmin = 0; ndmax = 10; nodmax = 10; radius =25; skmean = 0
 tmin = -999; tmax = 999
@@ -378,6 +390,9 @@ plt.savefig('sim50flat_Cv.png', bbox_inches='tight', dpi=300, frameon='false', p
 ####################### H / 75 peat / 25 sand / No hummocks  #######################
 ####################################################################################
 # Sequential Indicator Simulation with Simple Kriging Multiple Realizations 
+# A: 0.7523 / 0.2477 
+# B: 0.7435 / 0.2565
+# C: 0.7518 / 0.2482
 
 nx = 500; ny = 30; xsiz =1; ysiz = 1; xmn = 0.5; ymn = 0.5; nxdis = 1; nydis = 1
 ndmin = 0; ndmax = 10; nodmax = 10; radius =25; skmean = 0
@@ -462,6 +477,11 @@ plt.savefig('sim75flat_C.png', bbox_inches='tight', dpi=300, frameon='false', pa
 ####################################################################################
 ####################### V / 75 peat / 25 sand / No hummocks  #######################
 ####################################################################################
+# Sequential Indicator Simulation with Simple Kriging Multiple Realizations 
+# A: 0.7421 / 0.2579 = PEAT / SAND
+# B: 0.7563 / 0.2437
+# C: 0.7545 / 0.2455
+
 nx = 500; ny = 30; xsiz =1; ysiz = 1; xmn = 0.5; ymn = 0.5; nxdis = 1; nydis = 1
 ndmin = 0; ndmax = 10; nodmax = 10; radius =25; skmean = 0
 tmin = -999; tmax = 999
@@ -482,7 +502,7 @@ sim75flatAv = geostats.sisim(df,'X','Y','Facies',ivtype=0,koption=0,ncut=2,thres
 
 sim75flatBv = geostats.sisim(df,'X','Y','Facies',ivtype=0,koption=0,ncut=2,thresh=thresh,gcdf=gcdf,trend=dummy_trend,
                tmin=tmin,tmax=tmax,zmin=0.0,zmax=1.0,ltail=1,ltpar=1,middle=1,mpar=0,utail=1,utpar=1,
-               nx=nx,xmn=xmn,xsiz=xsiz,ny=ny,ymn=ymn,ysiz=ysiz,seed = 64961,
+               nx=nx,xmn=xmn,xsiz=xsiz,ny=ny,ymn=ymn,ysiz=ysiz,seed = 64997,
                ndmin=ndmin,ndmax=ndmax,nodmax=nodmax,mults=1,nmult=3,noct=-1,radius=radius,ktype=0,vario=varios)
 
 sim75flatCv = geostats.sisim(df,'X','Y','Facies',ivtype=0,koption=0,ncut=2,thresh=thresh,gcdf=gcdf,trend=dummy_trend,
@@ -490,7 +510,7 @@ sim75flatCv = geostats.sisim(df,'X','Y','Facies',ivtype=0,koption=0,ncut=2,thres
                nx=nx,xmn=xmn,xsiz=xsiz,ny=ny,ymn=ymn,ysiz=ysiz,seed = 65081,
                ndmin=ndmin,ndmax=ndmax,nodmax=nodmax,mults=1,nmult=3,noct=-1,radius=radius,ktype=0,vario=varios)
 
-# 65103, 64997
+# 65103, 64961
 
 plt.hist(sim75flatAv.flatten(),bins=2,density=True,cumulative = True,alpha=0.1,color='red',edgecolor='black')
 plt.hist(sim75flatBv.flatten(),bins=2,density=True,cumulative = True,alpha=0.1,color='red',edgecolor='black')
@@ -503,14 +523,7 @@ sim75flat_exCv = expand(sim75flatCv,4)
 
 # Plot the simulations for the dxf conversion
 plt.figure(figsize=(20*1.8395879323031639,1.2*1.8410852713178292))                                    # plot the results
-fig=plt.imshow(simfl75at_exAv, cmap='Greys_r')
-plt.axis('off')
-fig.axes.get_xaxis().set_visible(False)
-plt.savefig('sim75flat_ink_Av.png', bbox_inches='tight', dpi=300, frameon='false', pad_inches=0)
-
-# Plot the simulations for the dxf conversion
-plt.figure(figsize=(20*1.8395879323031639,1.2*1.8410852713178292))                                    # plot the results
-fig=plt.imshow(sim75flat_exAv, cmap='Greys_r')
+fig=plt.imshow(sim75flatAv, cmap='Greys_r')
 plt.axis('off')
 fig.axes.get_xaxis().set_visible(False)
 plt.savefig('sim75flat_ink_Av.png', bbox_inches='tight', dpi=300, frameon='false', pad_inches=0)
@@ -551,6 +564,9 @@ plt.savefig('sim75flat_Cv.png', bbox_inches='tight', dpi=300, frameon='false', p
 ######################### H / 25 peat / 75 sand / hummocks  ########################
 ####################################################################################
 # Sequential Indicator Simulation with Simple Kriging Multiple Realizations 
+# A: 0.2548 / 0.7452 = PEAT / SAND
+# B: 0.2558 / 0.7442
+# C: 0.2446 / 0.7554
 
 nx = 500; ny = 15; xsiz =1; ysiz = 1; xmn = 0.5; ymn = 0.5; nxdis = 1; nydis = 1
 ndmin = 0; ndmax = 10; nodmax = 10; radius =25; skmean = 0
@@ -580,15 +596,14 @@ sim25hum1025C = geostats.sisim(df,'X','Y','Facies',ivtype=0,koption=0,ncut=2,thr
                nx=nx,xmn=xmn,xsiz=xsiz,ny=ny,ymn=ymn,ysiz=ysiz,seed = 64969,
                ndmin=ndmin,ndmax=ndmax,nodmax=nodmax,mults=1,nmult=3,noct=-1,radius=radius,ktype=0,vario=varios)
 
-
+# 64919
 # 64923, 64937, 67123, 67157
-# 65051, 65039, 65031, 64969, 64929, 64919, 64917, 67383
+# 65051, 65039, 65031, 64969, 64929, 64917, 67383
 
 # Expand the simulation by a factor of 4
 sim25hum1025_exA = expand(sim25hum1025A,4)
 sim25hum1025_exB = expand(sim25hum1025B,4)
 sim25hum1025_exC = expand(sim25hum1025C,4)
-
 
 # Make hummocks
 dist  = np.arange(0, sim25hum1025_exA.shape[1], 1)  # Assign sine wave x values (1 per cm)
@@ -599,7 +614,7 @@ top_surf  = hum_h*np.sin((2/hum_p)*np.pi*dist)+50   # Create sine wave for hummo
 plt.plot(dist, top_surf)
 # Create a disturbed peat surface to match the hummock geometry
 # Create cosine wave for disturbed bottom surface 
-bot_surf  = hum_h*np.cos((2/hum_p)*np.pi*dist)+10
+bot_surf  = hum_h*np.sin((2/hum_p)*np.pi*dist)+10
 
 #def integrand(dist):
 #    return (hum_h*np.sin((2/hum_p)*np.pi*dist)+50)- (10*np.sin((2/50)*np.pi*dist)+10)
@@ -664,3 +679,32 @@ fig=plt.imshow(sim50flat_exC, cmap='Greys_r')
 plt.savefig('sim25hum1025_C.png', bbox_inches='tight', dpi=300, frameon='false', pad_inches=0)
 
 
+
+
+
+
+
+
+
+
+
+# Make hummocks
+dist  = np.arange(0, sim25hum1025_exA.shape[1], 1)  # Assign sine wave x values (1 per cm)
+hum_h=40                                            # Define hummock height (in cm)
+hum_p=200                                        # Define hummock period 
+top_surf  = hum_h*np.sin((2/hum_p)*np.pi*dist)+50   # Create sine wave for hummocky top surface 
+
+# Plot a sine wave using time and amplitude obtained for the sine wave
+plt.plot(dist, top_surf)
+
+# Create a disturbed peat surface to match the hummock geometry
+# Create cosine wave for disturbed bottom surface 
+bot_surf  = hum_h*np.cos((2/hum_p)*np.pi*dist-0.7)+10
+def integrand(dist):
+    return (hum_h*np.sin((2/hum_p)*np.pi*dist)+50)- (hum_h*np.cos((2/hum_p)*np.pi*dist-0.5)+10)
+integrate.quad(integrand, 0, 2000)
+
+plt.figure(figsize=(20*1.8395879323031639,1*1.8410852713178292))                                    # plot the results
+fig=plt.plot(dist, bot_surf, 'k', linewidth=0.5)
+plt.plot(dist, top_surf, 'k', linewidth=0.5)
+plt.savefig('large_hums.png', bbox_inches='tight', dpi=300, frameon='false', pad_inches=0)
