f1ed8b4bcf8a1a2124d540cfa983b57135d3db0f
Erika Ito <etito@wisc.edu>
2020-01-08 21:31:19 -0500

    Update Figs.py

--- Figs.py
+++ Figs.py
@@ -1,8 +1,25 @@
+# Import Libraries
+import os                                                 
+import numpy as np                                      
+import matplotlib.pyplot as plt     
+
+# Go to directory for figures 
+os.chdir("C:/Users/erikai94/Documents/UMass/Thesis/Figures")  
+
+plt.figure(figsize=(6,4))  
 x2  = np.arange(10,36, 1)
 y2=np.repeat(1, 26)
 x1  = np.arange(0,11, 1)
 y1=1*(1.5*(x1/10)-0.5*((x1/10)**3))
-plt.plot(x1,y1,color = 'green',label = '090')
-plt.plot(x2,y2,color = 'green',label = 'vario')
+x_range=np.repeat(10,2)
+y_range=(0,1)
+x_sill=(0,36)
+y_sill=np.repeat(1,2)
+plt.plot(x1,y1,color = 'black')
+plt.plot(x2,y2,color = 'black')
+plt.plot(x_range, y_range, color= 'black', ls='--')
+plt.plot(x_sill, y_sill, color= 'black', ls='--')
 plt.ylim([0,1.1])
 plt.xlim([0,35])
+plt.xticks(np.arange(0, 100,100 ))
+plt.savefig('Sph_Vario.png', dpi=300)
