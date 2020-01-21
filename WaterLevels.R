ce17bff4c23c11eefd74deebcf7d0c9243f4010e
Erika Ito <etito@wisc.edu>
2020-01-20 19:55:58 -0500

    finish pz 05 full

--- WaterLevels.R
+++ WaterLevels.R
@@ -933,7 +933,9 @@ sd(na.omit(TW_Grad_04_FULL2[,"dh/dz"])) # 0.03926074 m
 ############## TW_PZ_05 ##############   
 # Create a column for the depth to water below ground surface
 # The top of piezometer casing to ground surface = 25.75 cm
-TW_PZ_05_Nov2018[,"m_below_GS"]<-157/100-(25.75/100+TW_PZ_05_Nov2018[,"m_water"])             
+TW_PZ_05_Nov2018[,"m_below_GS"]<-157/100-(25.75/100+TW_PZ_05_Nov2018[,"m_water"])   
+# Remove erroneous rows of data 
+TW_PZ_05_Nov2018<-TW_PZ_05_Nov2018[which(TW_PZ_05_Nov2018[,"Date_Time"]=="06/19/18 03:15:00 PM"):which(TW_PZ_05_Nov2018[,"Date_Time"]=="11/19/18 04:00:00 PM"),]          
 # Smooth the spikes in the data caused by logger removal during data downloading
 # Interpolate between the 7/11 spike
 Start_Spike<-which(TW_PZ_05_Nov2018[,"Date_Time"]=="07/11/18 02:30:00 PM")                     
@@ -941,11 +943,9 @@ Stop_Spike<-which(TW_PZ_05_Nov2018[,"Date_Time"]=="07/11/18 03:00:00 PM")
 Smoothed_Spike<-seq(TW_PZ_05_Nov2018[Start_Spike,"m_below_GS"], TW_PZ_05_Nov2018[Stop_Spike,"m_below_GS"], length=Stop_Spike-Start_Spike+1)
 # Replace the spike with the smoothed interpolated data
 TW_PZ_05_Nov2018[Start_Spike:Stop_Spike,"m_below_GS"]<-Smoothed_Spike
-# Remove the beginning of the dataset (where logger was not submerged) 
-TW_PZ_05_Nov2018<-TW_PZ_05_Nov2018[which(TW_PZ_05_Nov2018[,"Date_Time"]=="06/19/18 03:15:00 PM"):nrow(TW_PZ_05_Nov2018),]
 # Add manual data
 TW_PZ_05_Nov2018[which(TW_PZ_05_Nov2018[,"Date_Time"]=="07/11/18 02:30:00 PM"),"m_manual"]<-0.298
-TW_PZ_05_Nov2018[which(TW_PZ_05_Nov2018[,"Date_Time"]=="11/19/18 03:00:00 PM"),"m_manual"]<-0.114
+TW_PZ_05_Nov2018[which(TW_PZ_05_Nov2018[,"Date_Time"]=="11/19/18 04:00:00 PM"),"m_manual"]<-0.114
 # Save as CSV  
 write.csv(TW_PZ_05_Nov2018, file="TWPZ05_6-19-18_to_11-19-18.csv", row.names=FALSE)    
                                                                      
@@ -961,15 +961,29 @@ write.csv(TW_PZ_05_3_3, file="TWPZ05_8-29-17_to_3-3-18.csv", row.names=FALSE)
                  
 # Create a column for the depth to water below ground surface
 # The top of piezometer casing to ground surface = 25.75 cm
-TW_PZ_05_Jun319[,"m_below_GS"]<-157/100-(25.75/100+TW_PZ_05_Jun319[,"m_water"])             
-# Remove the beginning of the dataset (where logger was not submerged)
-TW_PZ_05_Jun319<-TW_PZ_05_Jun319[which(TW_PZ_05_Jun319[,"Date_Time"]=="11/19/18 03:30:00 PM"):nrow(TW_PZ_05_Jun319),]                                    
+TW_PZ_05_Jun319[,"m_below_GS"]<-157/100-(25.75/100+TW_PZ_05_Jun319[,"m_water"])     
+# Remove erroneous rows of data 
+TW_PZ_05_Jun319<-TW_PZ_05_Jun319[which(TW_PZ_05_Jun319[,"Date_Time"]=="11/19/18 04:15:00 PM"):which(TW_PZ_05_Jun319[,"Date_Time"]=="06/03/19 11:00:00 AM"),]                                                 
 # Add manual data
 TW_PZ_05_Jun319[which(TW_PZ_05_Jun319[,"Date_Time"]=="03/30/19 12:30:00 PM"),"m_manual"]<-0.083
 TW_PZ_05_Jun319[which(TW_PZ_05_Jun319[,"Date_Time"]=="06/03/19 11:00:00 AM"),"m_manual"]<-0.129
 # Save as CSV  
 write.csv(TW_PZ_05_Jun319, file="TWPZ05_11-19-18_to_6-3-19.csv", row.names=FALSE)    
-          
+                    
+# Bind all of the data frames together...                  
+TW_PZ_05_FULL<-rbind(TW_PZ_05_Nov2018, TW_PZ_05_Jun319)  
+# Add tag in notes for where WL was recovering from isotope sampling
+# Recovery from 7/11 sampling: 
+TW_PZ_05_FULL[which(TW_PZ_05_FULL[,"Date_Time"]=="07/11/18 02:45:00 PM"):which(TW_PZ_05_FULL[,"Date_Time"]=="10/01/18 12:00:00 AM"),"Notes"]<-"sampling recovery"
+# Recovery from 3/30/19 sampling
+TW_PZ_05_FULL[which(TW_PZ_05_FULL[,"Date_Time"]=="03/30/19 03:30:00 PM"):which(TW_PZ_05_FULL[,"Date_Time"]=="05/01/19 12:00:00 AM"),"Notes"]<-"sampling recovery"
+# Calculate mean height above GS and the standard deviation of the height above GS:  
+# NOTE: we are ignoring the rows that are tagged with "sampling recovery" 
+mean(na.omit(TW_PZ_05_FULL[-which(TW_PZ_05_FULL[,"Notes"]=="sampling recovery"),"m_below_GS"])) # 0.1212639 m
+sd(na.omit(TW_PZ_05_FULL[-which(TW_PZ_05_FULL[,"Notes"]=="sampling recovery"),"m_below_GS"])) # 0.1065326 m  
+# Save as CSV  
+write.csv(TW_PZ_05_FULL, file="TWPZ05_FULL.csv", row.names=FALSE)  
+      
 ############## TW_PZ_05_SAND ##############  
 # June 2018
 TW_PZ_05_SAND_Jun2018[,"m_below_GS"]<-153/100-(105/100+TW_PZ_05_SAND_Jun2018[,"m_water"])  
@@ -1927,6 +1941,15 @@ ggsave("TW_PZ_05_11-19-18_to_6-3-19.pdf", width = 12, height = 6)
 Plot_Times<-as.POSIXct(TW_PZ_05_Jun319[,"Date_Time"], "%m/%d/%y %I:%M:%S %p", tz="America/New_York")
 ggplot(TW_PZ_05_Jun319, aes(Plot_Times, TW_PZ_05_Jun319[,"m_below_GS"]))+geom_line(color='royalblue3', size=.6) + xlab("Date") + ylab("Depth to Water Below Ground Surface (m)")+ggtitle("TW_PZ_05")+  scale_x_datetime(breaks = seq(Plot_Times[1], Plot_Times[length(Plot_Times)], "7 days"),date_labels="%b %d")+ scale_y_reverse(limits =c(1.0,-0.3)) +theme(axis.text.x = element_text(angle=45, vjust = 0.5))  + geom_point(aes(x=Plot_Times, y=TW_PZ_05_Jun319[,"m_manual"]), color="orange3", size=3)                         
 ggsave("TW_PZ_05_11-19-18_to_6-3-19_manual.pdf", width = 12, height = 6)
+
+# Complete PZ_01 dataset
+Plot_Times<-as.POSIXct(TW_PZ_01_FULL[,"Date_Time"], "%m/%d/%y %I:%M:%S %p", tz="America/New_York")
+ggplot(TW_PZ_05_FULL, aes(Plot_Times, TW_PZ_05_FULL[,"m_below_GS"]))+geom_line(color='royalblue3', size=.3) + xlab("Date") + ylab("Depth to Water Below Ground Surface (m)")+ggtitle("TW_PZ_05")+  scale_x_datetime(breaks = seq(as.POSIXct("2018-05-01 00:00:00 EDT"), as.POSIXct("2019-09-01 00:00:00 EDT"), "1 month"),date_labels="%b %d, %Y")+ scale_y_reverse(limits =c(.9,-.4)) +theme(axis.text.x = element_text(angle=45, vjust = 0.5))
+ggsave("TW_PZ_05_FULL.pdf", width = 12, height = 6)                             
+# Complete with manual
+Plot_Times<-as.POSIXct(TW_PZ_05_FULL[,"Date_Time"], "%m/%d/%y %I:%M:%S %p", tz="America/New_York")
+ggplot(TW_PZ_05_FULL, aes(Plot_Times, TW_PZ_05_FULL[,"m_below_GS"]))+geom_line(color='royalblue3', size=.3) + xlab("Date") + ylab("Depth to Water Below Ground Surface (m)")+ggtitle("TW_PZ_05")+  scale_x_datetime(breaks = seq(as.POSIXct("2018-05-01 00:00:00 EDT"), as.POSIXct("2019-09-01 00:00:00 EDT"), "1 month"),date_labels="%b %d, %Y")+ scale_y_reverse(limits =c(.9,-.4)) +theme(axis.text.x = element_text(angle=45, vjust = 0.5))+ geom_point(aes(x=Plot_Times, y=TW_PZ_05_FULL[,"m_manual"]), color="orange3", size=3) 
+ggsave("TW_PZ_05_FULL_manual.pdf", width = 12, height = 6)                             
 
 ################ TW_PZ_05_SAND ################
 # November 2018
